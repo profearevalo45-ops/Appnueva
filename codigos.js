c7c89ee12b27ef1808da264e50e0fe2efcbf5fa1
profearevalo45-ops <profearevalo45@gmail.com>
2026-09-20 15:48:50 -0300

    Update codigos.js

--- codigos.js
+++ codigos.js
@@ -1,16 +1,3 @@
-appfiles-three.vercel.app
-appfiles-three.vercel.app
-https://us.wps.com/cms/docs/d/cbEaajk5dAOq7UfF?lg=es-MX&sa=601.1074&ps=1&fn=codigos-para-entregar.txt&refer=whatsapp
-
-Abrir en la app:
-https://go.wps.com/u8J2m0CQXaTG
-Código de acceso a la app planillas
-github.com/profearevalo45-ops/Appnueva/upload
-https://us.wps.com/cms/docs/d/cbEaaazxN8MegXPw?lg=es-MX&sa=601.1074&ps=1&fn=codigos.js&refer=whatsapp
-
-Abrir en la app:
-https://go.wps.com/ubHc5BJIAVpx
-github.com/profearevalo45-ops/Appnueva/upload
 /* Códigos de acceso válidos de "Planillas y Organización Escolar".
    Acá no están los códigos: están sus "huellas", que no se pueden leer ni copiar.
    - Dar de baja a una compradora: borrá su línea (la que tiene su número) y guardá.
@@ -22,4 +9,34 @@ window.CODIGOS_VALIDOS = [
   "4c9f578a2e05cce6dd5a8f40c2aaade27829d0a6ec5bf865d1c7c5955a5ee9a7",  // 02
   "bf6b4718f80668fa505cde0f3af1675815c60a1dddd437a0e339e263a918e4e3",  // 03
   "32e68d0d89ad42f9816c0bbf0d03a2d539d8d5ac97969ffb7217dd912bcb3d84",  // 04
-  …
+  "b5e404c2ea884479c27ab4f093d3b63140956bebe1f77d111b34899f326bbd10",  // 05
+  "af40a15548f5d51daa32849fb53b09f578c8d89809f909506b924edc3103c1c0",  // 06
+  "c9af01f3df02dac49c04204db273eb62104d93cbe697d7157aecbff6379844ac",  // 07
+  "21cb33be979f226e8506a65b55610049959cf0ecea759e4fe526e2184ab0efff",  // 08
+  "16065a0feb91d542bcf379b1bb21d16d466334ff80114db38f9ec3fae39d54f7",  // 09
+  "5c1ca17a521eb4e333c567917f5e9a42229d061e71357e13df4db41967f2592c",  // 10
+  "63571a96c7789e86ddd3bc9516e200a7bbd0f396ebe5b38e9ca86e05629f337f",  // 11
+  "86ad3125fbeee32b440957e44cb7615ded068334208da2b91df75333a341cd15",  // 12
+  "5d2aa1ec6a624a60d324fdf22cf87ff13941dc6853f1529efbba89daccf9cb1c",  // 13
+  "26595c3a5798ed6315607b9f1535ce25041a63583dead3396a2732011ceedd0c",  // 14
+  "7d6a16803cc78adbabd5558f9a0991712f96e3b125dc94970b66cf957dd58a1f",  // 15
+  "a33b730ff54221c8c3a54cf390e4c8c441afe989358d3221abba3b4371bf571b",  // 16
+  "cf04da53752ccd20c1dc9b3713668dd6921b685ea468b49efbd508fb5691294f",  // 17
+  "ccebb21224c4518c3d544fbb1bd843018ce9b8523c510178be416421e25f7d2e",  // 18
+  "6103bdfa72bd9fd92cb1c066a064683d33c8cf04f3ecc7822ccd4a6bbb3ecc57",  // 19
+  "f973b27885970a41cd90b8b91a14edb020ba6ad253651ff1b6966be03fd55b06",  // 20
+  "06781506bc2c073fb05b4a8ad3ba68dd10ccbcccddc97e6e48868b2e5ff4faef",  // 21
+  "8da094db2be7287960d138757fbdf32d08fbd988e4c3e2daad1e6d7ef858cff8",  // 22
+  "5aa5e76bdc0572d5b472a2a3413068e17b21c4125ae1739889150da3bb3379b3",  // 23
+  "99263f6485a9e0e2f524c6e930eba5826fa7d0fb277f694d0063e4bb0892bbaf",  // 24
+  "b781486857792785833fd59bc81f35620819b7711f9afa717c2135d7c8f2a40b",  // 25
+  "703cb395ce43c4b8875ecc221e546ecc1541d63bcd8f90e1f6b81db2f1825af9",  // 26
+  "4619545a6536e77fbac2bd4af5fd4db97ae2d8143eb54fbb040df062b4efc399",  // 27
+  "fdd83bf6cecf68ed69fef560e5233516fccd9434baa8a0709b6ea7504878595b",  // 28
+  "10971f7211aa02ab2c8e31874ae255f3b56e30043dd927adfd923fd3b680b29d",  // 29
+  "8e37d83a187f5496bb05a09f9c40530c153d8fa950b6b9958d6287153cff63f6",  // 30
+];
+
+/* Cómo contactarte. Aparece en la pantalla de acceso cuando alguien no tiene
+   código o su acceso se dio de baja. Ejemplo: "WhatsApp 11 2345 6789". */
+window.CONTACTO_ACCESO = "";
