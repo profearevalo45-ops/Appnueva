89e8b8d6d0421fa2cba7a577471b9bad0d2740b9
profearevalo45-ops <profearevalo45@gmail.com>
2026-09-20 15:40:20 -0300

    Create codigos.js

---
+++ codigos.js
@@ -0,0 +1,25 @@
+appfiles-three.vercel.app
+appfiles-three.vercel.app
+https://us.wps.com/cms/docs/d/cbEaajk5dAOq7UfF?lg=es-MX&sa=601.1074&ps=1&fn=codigos-para-entregar.txt&refer=whatsapp
+
+Abrir en la app:
+https://go.wps.com/u8J2m0CQXaTG
+Código de acceso a la app planillas
+github.com/profearevalo45-ops/Appnueva/upload
+https://us.wps.com/cms/docs/d/cbEaaazxN8MegXPw?lg=es-MX&sa=601.1074&ps=1&fn=codigos.js&refer=whatsapp
+
+Abrir en la app:
+https://go.wps.com/ubHc5BJIAVpx
+github.com/profearevalo45-ops/Appnueva/upload
+/* Códigos de acceso válidos de "Planillas y Organización Escolar".
+   Acá no están los códigos: están sus "huellas", que no se pueden leer ni copiar.
+   - Dar de baja a una compradora: borrá su línea (la que tiene su número) y guardá.
+   - Agregar códigos nuevos: usá generar-codigos.html y pegá las líneas nuevas
+     antes del cierre "];".
+   Los números (01, 02...) son solo para que ubiques cada línea. */
+window.CODIGOS_VALIDOS = [
+  "6876a7703fa9ccf4da98587e307cfd10d590c1db5a04430dc359d4f66d251f0a",  // 01 - Marielis (propia)
+  "4c9f578a2e05cce6dd5a8f40c2aaade27829d0a6ec5bf865d1c7c5955a5ee9a7",  // 02
+  "bf6b4718f80668fa505cde0f3af1675815c60a1dddd437a0e339e263a918e4e3",  // 03
+  "32e68d0d89ad42f9816c0bbf0d03a2d539d8d5ac97969ffb7217dd912bcb3d84",  // 04
+  …
